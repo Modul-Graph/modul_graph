6d91edad3c2f225a6178045fbe03895bcef9388f
rpossardt <rune.possardt@st.ovgu.de>
2024-06-07 13:41:55 +0200

    feat: standardize comments

--- modul_graph/models/competence.py
+++ modul_graph/models/competence.py
@@ -2,8 +2,8 @@ from neomodel import StructuredNode, StringProperty, RelationshipTo, ZeroOrMore,
 import unstructured_entry
 
 class Competence(StructuredNode):
-    #attribute
     name = StringProperty(required=True, unique=True)
-    #relationships
+
+    # connection to Module
     is_needed_by = RelationshipFrom('Module', 'NEEDS', cardinality=ZeroOrMore)
     is_provided_by = RelationshipTo('Module', 'PROVIDES', cardinality=OneOrMore)
