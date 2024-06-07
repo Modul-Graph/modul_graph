4eb885dae2c362508a57ab36631603e650bfd461
rpossardt <rune.possardt@st.ovgu.de>
2024-06-07 13:45:54 +0200

    feat: combine all node implementations in one branch

--- modul_graph/models/micro_unit.py
+++ modul_graph/models/micro_unit.py
@@ -5,6 +5,6 @@ class MicroUnit(StructuredNode):
     name = StringProperty(required=True, unique_index=True)
     level = IntegerProperty(required=True)
 
-    #connection to Module
+    # connection to Module
     needs_from_module = RelationshipFrom('Module', 'NEEDS', cardinality=ZeroOrMore)
     provide_to_module = RelationshipTo('Module', 'PROVIDES', cardinality=OneOrMore)
