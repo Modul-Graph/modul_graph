146026305bf4749e524ea10f04bb05ffeca3452c
Finn Hering <finn@hering.sh>
2024-06-18 23:10:28 +0000

    feat: add modules and competences

---
+++ modul_graph/static/spo_2017_inf_wise/_competences.py
@@ -0,0 +1,111 @@
+from modul_graph.models.competence import Competence
+
+algorithmen_und_datenstrukturen = Competence()
+algorithmen_und_datenstrukturen.name = "Algorithmen und Datenstrukturen"
+algorithmen_und_datenstrukturen.save()
+
+analysis = Competence()
+analysis.name = "Analysis"
+analysis.save()
+
+numerik = Competence()
+numerik.name = "Numerik"
+numerik.save()
+
+betriebssysteme = Competence()
+betriebssysteme.name = "Betriebssysteme"
+betriebssysteme.save()
+
+datenbanken_und_informationssysteme = Competence()
+datenbanken_und_informationssysteme.name = "Datenbanken und Informationssysteme"
+datenbanken_und_informationssysteme.save()
+
+digitaltechnik_und_rechnerorganisation = Competence()
+digitaltechnik_und_rechnerorganisation.name = "Digitaltechnik und Rechnerorganisation"
+digitaltechnik_und_rechnerorganisation.save()
+
+diskrete_strukturen = Competence()
+diskrete_strukturen.name = "Diskrete Strukturen"
+diskrete_strukturen.save()
+
+logik = Competence()
+logik.name = "Logik"
+logik.save()
+
+algebra = Competence()
+algebra.name = "Algebra"
+algebra.save()
+
+lineare_algebra = Competence()
+lineare_algebra.name = "lineare Algebra"
+lineare_algebra.save()
+
+formale_sprachen_und_automaten = Competence()
+formale_sprachen_und_automaten.name = "Formale Sprachen und Automaten"
+formale_sprachen_und_automaten.save()
+
+informatik_als_disziplin = Competence()
+informatik_als_disziplin.name = "Informatik als Disziplin"
+informatik_als_disziplin.save()
+
+informatik_und_gesellschaft = Competence()
+informatik_und_gesellschaft.name = "Informatik und Gesellschaft"
+informatik_und_gesellschaft.save()
+
+it_sicherheit = Competence()
+it_sicherheit.name = "IT-Sicherheit"
+it_sicherheit.save()
+
+mensch_computer_interaktion = Competence()
+mensch_computer_interaktion.name = "Mensch-Computer-Interaktion"
+mensch_computer_interaktion.save()
+
+modellierung = Competence()
+modellierung.name = "Modellierung"
+modellierung.save()
+
+programmiersprachen_und_methodik = Competence()
+programmiersprachen_und_methodik.name = "Programmiersprachen und -methodik"
+programmiersprachen_und_methodik.save()
+
+projekt_und_teamkompetenz = Competence()
+projekt_und_teamkompetenz.name = "Projekt- und Teamkompetenz"
+projekt_und_teamkompetenz.save()
+
+rechnernetze_und_verteilte_systeme = Competence()
+rechnernetze_und_verteilte_systeme.name = "Rechnernetze und verteilte Systeme"
+rechnernetze_und_verteilte_systeme.save()
+
+software_engineering = Competence()
+software_engineering.name = "Software-Engineering"
+software_engineering.save()
+
+wahrscheinlichkeitstheorie = Competence()
+wahrscheinlichkeitstheorie.name = "Wahrscheinlichkeitstheorie"
+wahrscheinlichkeitstheorie.save()
+
+statistik = Competence()
+statistik.name = "Statistik"
+statistik.save()
+
+topologie = Competence()
+topologie.name = "Topologie"
+topologie.save()
+
+differentialgeometrie = Competence()
+differentialgeometrie.name = "Differentialgeometrie"
+differentialgeometrie.save()
+
+robotik = Competence()
+robotik.name = "Robotik"
+robotik.save()
+
+künstliche_intelligenz = Competence()
+künstliche_intelligenz.name = "Künstliche Intelligenz"
+künstliche_intelligenz.save()
+
+analytische_geometrie = Competence()
+analytische_geometrie.name = "Analytische Geometrie"
+analytische_geometrie.save()
+
+
