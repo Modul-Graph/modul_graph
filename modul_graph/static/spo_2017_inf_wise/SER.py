c8022658923adf334cf540d8e3cab6e6af8fa1ae
Finn Hering <finn@hering.sh>
2024-06-21 11:50:24 +0000

    fix: connect modules to SER

--- modul_graph/static/spo_2017_inf_wise/SER.py
+++ modul_graph/static/spo_2017_inf_wise/SER.py
@@ -1,5 +1,9 @@
+from modul_graph.models.module import Module
 from modul_graph.models.study_exam_rules import StudyExamRules
 from modul_graph.static.spo_2017_inf_wise._semesters import semesters
+import modul_graph.static.spo_2017_inf_wise._modules as _modules
+
+
 
 spo_2017_inf_wise = StudyExamRules()
 spo_2017_inf_wise.name = "SPO 2017 Informatik (Start Wintersemester)"
@@ -7,3 +11,7 @@ spo_2017_inf_wise.save()
 
 for s in semesters:
     spo_2017_inf_wise.specifies_semester.connect(s)
+
+for v in _modules.__dict__.values():
+    if isinstance(v, Module):
+        v.belongs_to_SER.connect(spo_2017_inf_wise)
