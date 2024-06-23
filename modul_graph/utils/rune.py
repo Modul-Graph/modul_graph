0684553bc121c370fda350a00570600ab5a0ba06
rpossardt <rune.possardt@st.ovgu.de>
2024-06-23 19:17:28 +0200

    fix: test DB connection

--- modul_graph/utils/rune.py
+++ modul_graph/utils/rune.py
@@ -1,13 +1,12 @@
 from neomodel import db
-
-
 # Funktionen in __main__.py schreiben (nach Dateneintrag in DB)
 # console: python -m modul_graph
+
+
 def test_db_connection() -> bool:
     result, meta = db.cypher_query('MATCH p=(:Module)-[r:PROVIDES]->(:Competence) RETURN p', resolve_objects=True)
-    print("TEST")
-    print(result)
-    print(meta)
+    if len(result) > 1:
+        return False
     return True
 
 
