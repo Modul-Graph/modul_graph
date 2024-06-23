d62b453d287d8ec6906bd05e08e5910b118e3394
rpossardt <rune.possardt@st.ovgu.de>
2024-06-23 18:54:25 +0200

    feat: add start semester attribute to study_exam_rules

--- modul_graph/models/study_exam_rules.py
+++ modul_graph/models/study_exam_rules.py
@@ -1,8 +1,9 @@
-from neomodel import StructuredNode, StringProperty, OneOrMore, RelationshipTo, ZeroOrMore, IntegerProperty, One, RelationshipFrom
+from neomodel import StructuredNode, StringProperty, OneOrMore, RelationshipTo, ZeroOrMore, IntegerProperty, One, RelationshipFrom, BooleanProperty
 
 
 class StudyExamRules(StructuredNode):
     name = StringProperty(required=True, unique=True)
+    start_winter = BooleanProperty()
 
     # connection to Module
     has_module = RelationshipFrom('modul_graph.models.module.Module', 'BELONGS_TO', cardinality=OneOrMore)
