84b9b8d69d480cacca9a673d0162b9b8fbc49e46
Finn Hering <finn@hering.sh>
2024-08-16 15:53:27 +0200

    chore: move service to analysis_service

--- modul_graph/utils/analysis_service.py
+++ modul_graph/utils/analysis_service.py
@@ -12,10 +12,53 @@ from .std_curr import std_curr, instantiate_std_curr_obj  # type: ignore
 from collections import Counter
 from random import shuffle
 from .graph_exception import GraphException  # type: ignore
+from ..DTOs import PflichtmoduleDTO, WpfDTO
 
 
 # service provides controller with processed data and gets its data from data_access (NOT from repository)
 
+
+def get_competence_sc(sc: StandardCurriculum) -> CompetenceScDTO:
+    """
+    Get competence standard curriculum for standard curriculum
+    :param sc: standard curriculum to get the competence standard curriculum for
+    :return: competence standard curriculum
+    """
+
+    sem_ma = da_get_semester_to_module_area_for_standard_curriculum(sc)
+    pflichtmodule: list[PflichtmoduleDTO] = []
+    name_wpf: dict[str, WpfDTO] = {}
+
+    for semester, module_areas in sem_ma.items():
+        for module_area in module_areas:
+
+            # Handle WPF part
+            if module_area.is_wpf:
+                modules: list[Module] = list(module_area.filled_by_module)
+
+                if name_wpf.get(module_area.name) is None:
+                    # initialize list of module competence dtos
+                    module_competence_dtos: list[ModuleCompetenceDTO] = []
+                    for module in modules:
+                        competences: list[Competence] = list(module.provides_competence)
+                        module_competence_dtos.append(
+                            ModuleCompetenceDTO(name=module.name,
+                                                competences=list(map(lambda x: x.name, competences)))
+
+                        )
+
+                    name_wpf[module_area.name] = WpfDTO(name=module_area.name, semesters={semester},
+                                                        modules=module_competence_dtos)
+                else:
+                    name_wpf[module_area.name].semesters.add(semester)
+            else:
+                # Handle Pflichtmodule part
+                module = list(module_area.filled_by_module)[0]
+                competences = list(module.provides_competence)
+                pflichtmodule.append(PflichtmoduleDTO(name=module.name, semester=semester,
+                                                      competences=list(map(lambda x: x.name, competences))))
+
+    return CompetenceScDTO(WPF=list(name_wpf.values()), pflichtmodule=pflichtmodule)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # public functions (accessed by controller)
--- modul_graph/utils/service.py
+++ modul_graph/utils/service.py
@@ -2,47 +2,6 @@ from modul_graph.DTOs import CompetenceScDTO, PflichtmoduleDTO, WpfDTO, ModuleCo
 from modul_graph.models.competence import Competence
 from modul_graph.models.module import Module
 from modul_graph.models.standard_curriculum import StandardCurriculum
-from modul_graph.utils.data_access import da_get_semester_to_module_area_for_standard_curriculum
 
 
-def get_competence_sc(sc: StandardCurriculum) -> CompetenceScDTO:
-    """
-    Get competence standard curriculum for standard curriculum
-    :param sc: standard curriculum to get the competence standard curriculum for
-    :return: competence standard curriculum
-    """
 
-    sem_ma = da_get_semester_to_module_area_for_standard_curriculum(sc)
-    pflichtmodule: list[PflichtmoduleDTO] = []
-    name_wpf: dict[str, WpfDTO] = {}
-
-    for semester, module_areas in sem_ma.items():
-        for module_area in module_areas:
-
-            # Handle WPF part
-            if module_area.is_wpf:
-                modules: list[Module] = list(module_area.filled_by_module)
-
-                if name_wpf.get(module_area.name) is None:
-                    # initialize list of module competence dtos
-                    module_competence_dtos: list[ModuleCompetenceDTO] = []
-                    for module in modules:
-                        competences: list[Competence] = list(module.provides_competence)
-                        module_competence_dtos.append(
-                            ModuleCompetenceDTO(name=module.name,
-                                                competences=list(map(lambda x: x.name, competences)))
-
-                        )
-
-                    name_wpf[module_area.name] = WpfDTO(name=module_area.name, semesters={semester},
-                                                        modules=module_competence_dtos)
-                else:
-                    name_wpf[module_area.name].semesters.add(semester)
-            else:
-                # Handle Pflichtmodule part
-                module = list(module_area.filled_by_module)[0]
-                competences = list(module.provides_competence)
-                pflichtmodule.append(PflichtmoduleDTO(name=module.name, semester=semester,
-                                                      competences=list(map(lambda x: x.name, competences))))
-
-    return CompetenceScDTO(WPF=list(name_wpf.values()), pflichtmodule=pflichtmodule)
