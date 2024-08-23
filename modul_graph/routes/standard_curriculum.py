bdfefe5dc89ecc368c3dd2e4a2ff41f9914f794e
rpossardt <rune.possardt@st.ovgu.de>
2024-08-23 13:42:01 +0200

    feat: route to get all module areas for standard curriculum

--- modul_graph/routes/standard_curriculum.py
+++ modul_graph/routes/standard_curriculum.py
@@ -4,8 +4,9 @@ This file contains all routes that get, transform or create standard curriculums
 from typing import List
 
 from fastapi import APIRouter
-from neomodel import NodeSet  # type: ignore
+from neomodel import NodeSet, CardinalityViolation  # type: ignore
 
+from modul_graph.models.module_area import ModuleArea
 from modul_graph.DTOs import StandardCurriculumDTO, CompetenceScDTO
 from modul_graph.models.standard_curriculum import StandardCurriculum
 from modul_graph.utils.analysis_controller import get_competence_sc as get_competence_sc_service
@@ -45,7 +46,26 @@ async def get_std_curr_modules(sc_name: str) -> List[str]:
 
     return res
 
-# todo: get all module areas
+
+@router.get("/get_module_areas")
+async def get_std_curr_module_areas(sc_name: str) -> List[str]:
+    res: List[str] = []
+
+    sc: StandardCurriculum = StandardCurriculum.nodes.get(name=sc_name)
+
+    for module in sc.has_module.all():
+        try:
+            for module_area in module.fills_module_area.all():
+                assert isinstance(module_area, ModuleArea)
+
+                res.append(module_area.name)
+        except CardinalityViolation:
+            # some modules don't fill module areas
+            # (even though they should)
+            # -> cardinality violated
+            pass
+
+    return res
 
 
 @router.get("/competence/{sc}", tags=["READ"])
